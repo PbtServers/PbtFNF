db66eef9dfea17c15fa7ddc92304b45bc7d7388e
Pbt <pbt@pbtservers.com>
2022-12-03 16:25:15 +0100

    Actualización Futura (Más Cosas Nuevas)

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -139,7 +139,7 @@ class MainMenuState extends MusicBeatState
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "PbtFNF v2.0.0", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "PbtFNF v2.1.0", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
