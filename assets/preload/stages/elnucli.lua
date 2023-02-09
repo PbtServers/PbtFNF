198a3eaec253f37398f5e5fb24891a945f0b4f20
Pbt <pbt@pbtservers.com>
2023-02-09 16:56:17 +0100

    Actualización Futura (Noche-de-Magic [Terminado!])

--- assets/preload/stages/elnucli.lua
+++ assets/preload/stages/elnucli.lua
@@ -2,8 +2,11 @@ function onCreate()
 	-- background shit
 	makeLuaSprite('stageback', 'elnucli', -600, -300);
 	setScrollFactor('stageback', 0.9, 0.9);
+	
+	makeAnimatedLuaSprite('cartarelampago', 'CartaMagicRelampago', -600, -300);
 
 	addLuaSprite('stageback', false);
+	addLuaSprite('cartarelampago', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
