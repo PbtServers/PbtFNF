be3df7b913bd1b75fad8ea61fdd5ba386f28a2c7
Pbt <pbt@pbtservers.com>
2023-02-10 00:05:08 +0100

    Actualización Futura --> Faltan Solo 2 Canciones [JugadorALaFuga y KilosMortales]

--- assets/preload/data/seguridad-aplicada/Script.lua
+++ assets/preload/data/seguridad-aplicada/Script.lua
@@ -27,6 +27,153 @@ local del2 = 0;
 
 function onUpdate()
 
+if curBeat == 0 then
+
+makeLuaSprite('pcendido', 'M6-Virus-PC', 0, 0);
+addLuaSprite('pcendido', true);
+
+end
+
+if curBeat == 2 then
+
+removeLuaSprite('pcendido', true);
+makeLuaSprite('pcwin', 'M6-Virus-Login', 0, 0);
+addLuaSprite('pcwin', true);
+
+end
+
+if curBeat == 4 then
+
+removeLuaSprite('pcwin', true);
+makeLuaSprite('pccrash', 'M6-Virus-Crash', 0, 0);
+addLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 5 then
+
+removeLuaSprite('pccrash', true);
+makeLuaSprite('pcmanos', 'M6-Virus-PC-Manos', 0, 0);
+addLuaSprite('pcmanos', true);
+
+end
+
+if curBeat == 7 then
+
+removeLuaSprite('pcmanos', true);
+
+end
+
+if curBeat == 90 then
+
+makeLuaSprite('pcendido', 'M6-Virus-PC', 0, 0);
+addLuaSprite('pcendido', true);
+
+end
+
+if curBeat == 97 then
+
+removeLuaSprite('pcendido', true);
+makeLuaSprite('pccrash', 'M6-Virus-Crash', 0, 0);
+addLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 100 then
+
+removeLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 214 then
+
+makeLuaSprite('pcendido', 'M6-Virus-PC', 0, 0);
+addLuaSprite('pcendido', true);
+
+end
+
+if curBeat == 221 then
+
+removeLuaSprite('pcendido', true);
+makeLuaSprite('pccrash', 'M6-Virus-Crash', 0, 0);
+addLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 293 then
+
+removeLuaSprite('pccrash', true);
+makeLuaSprite('pcwin', 'M6-Virus-Login', 0, 0);
+addLuaSprite('pcwin', true);
+
+end
+
+if curBeat == 296 then
+
+removeLuaSprite('pcwin', true);
+
+end
+
+if curBeat == 319 then
+
+makeLuaSprite('pccrash', 'M6-Virus-Crash', 0, 0);
+addLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 322 then
+
+removeLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 356 then
+
+makeLuaSprite('pc11', 'M6-Virus-PC-Win11', 0, 0);
+addLuaSprite('pc11', true);
+
+end
+
+if curBeat == 362 then
+
+removeLuaSprite('pc11', true);
+makeLuaSprite('pccrash', 'M6-Virus-Crash', 0, 0);
+addLuaSprite('pccrash', true);
+
+end
+
+if curBeat == 363 then
+
+removeLuaSprite('pccrash', true);
+makeLuaSprite('pcwin', 'M6-Virus-Login', 0, 0);
+addLuaSprite('pcwin', true);
+
+end
+
+if curBeat == 367 then
+
+removeLuaSprite('pcwin', true);
+makeLuaSprite('pcendido', 'M6-Virus-PC', 0, 0);
+addLuaSprite('pcendido', true);
+
+end
+
+if curBeat == 374 then
+
+removeLuaSprite('pcendido', true);
+makeLuaSprite('pcwin', 'M6-Virus-Login', 0, 0);
+addLuaSprite('pcwin', true);
+
+end
+
+if curBeat == 378 then
+
+removeLuaSprite('pcwin', true);
+makeLuaSprite('pcendido', 'M6-Virus-PC', 0, 0);
+addLuaSprite('pcendido', true);
+
+end
+
 if del > 0 then
 
 del = del - 1
